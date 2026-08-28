bd61d10dc22afd36f55475da8be9e0fd80fbf911
RozanRamadani <114994099+RozanRamadani@users.noreply.github.com>
2026-08-28 17:06:30 +0700

    fix student_request

--- TM-3/api-students/app/model/student_request.go
+++ TM-3/api-students/app/model/student_request.go
@@ -1,16 +1,13 @@
 package model
 
-// CreateStudentRequest adalah data yang diterima
-// ketika client membuat student baru.
 type CreateStudentRequest struct {
+	ID       string  `json:"id"`
 	NIM      string  `json:"nim"`
 	Name     string  `json:"name"`
 	Grade    float64 `json:"grade"`
 	IsActive bool    `json:"is_active"`
 }
 
-// ReplaceStudentRequest digunakan untuk PUT.
-// PUT mengganti seluruh data student.
 type ReplaceStudentRequest struct {
 	NIM      string  `json:"nim"`
 	Name     string  `json:"name"`
@@ -18,15 +15,9 @@ type ReplaceStudentRequest struct {
 	IsActive bool    `json:"is_active"`
 }
 
-// PatchStudentRequest digunakan untuk PATCH.
-// Pointer digunakan supaya kita bisa membedakan:
-//
-// field tidak dikirim  → nil
-// field dikirim false  → pointer ke false
-// field dikirim 0      → pointer ke 0
 type PatchStudentRequest struct {
-	NIM      *string  `json:"nim,omitempty"`
-	Name     *string  `json:"name,omitempty"`
-	Grade    *float64 `json:"grade,omitempty"`
-	IsActive *bool    `json:"is_active,omitempty"`
+	NIM      *string  `json:"nim"`
+	Name     *string  `json:"name"`
+	Grade    *float64 `json:"grade"`
+	IsActive *bool    `json:"is_active"`
 }
