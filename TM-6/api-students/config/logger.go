38c3f51b28681b7ba08892536999b2d41ef2fbc7
RozanRamadani <114994099+RozanRamadani@users.noreply.github.com>
2026-09-20 02:49:37 +0700

    fix

--- TM-6/api-students/config/logger.go
+++ TM-6/api-students/config/logger.go
@@ -1,74 +1,39 @@
 package config
 
 import (
-	"time"
-
-	"api-students/app/model"
-
-	"github.com/gofiber/fiber/v2"
-	"github.com/google/uuid"
+	"encoding/json"
+	"log"
 )
 
-type RequestLog struct {
-	RequestID string `json:"request_id"`
-	Method    string `json:"method"`
-	Path      string `json:"path"`
-	Status    int    `json:"status"`
-	Duration  string `json:"duration"`
+var logger *log.Logger
 
-	UserID int    `json:"user_id,omitempty"`
-	Role   string `json:"role,omitempty"`
+// InitLogger menginisialisasi logger aplikasi.
+func InitLogger() error {
+	logger = log.Default()
+
+	return nil
 }
 
-type RequestLogger struct {
-	logFunc func(RequestLog)
+// CloseLogger menutup logger.
+//
+// Logger saat ini menggunakan stdout/stderr milik log package,
+// sehingga tidak ada resource file yang perlu ditutup.
+func CloseLogger() {
+	logger = nil
 }
 
-func NewRequestLogger(
-	logFunc func(RequestLog),
-) *RequestLogger {
-	return &RequestLogger{
-		logFunc: logFunc,
-	}
-}
-
-func (m *RequestLogger) Handler(c *fiber.Ctx) error {
-
-	start := time.Now()
-
-	requestID := c.Get("X-Request-ID")
-
-	if requestID == "" {
-		requestID = uuid.NewString()
+// LogRequest mencatat informasi request dalam format JSON.
+func LogRequest(data interface{}) {
+	if logger == nil {
+		logger = log.Default()
 	}
 
-	c.Set("X-Request-ID", requestID)
+	payload, err := json.Marshal(data)
 
-	err := c.Next()
-
-	// Default untuk request yang belum terautentikasi.
-	userID := 0
-	role := ""
-
-	// RequireAuth menyimpan AuthUser ke Locals("user").
-	if user, ok := c.Locals("user").(model.AuthUser); ok {
-		userID = user.UserID
-		role = user.Role
+	if err != nil {
+		logger.Printf("request log error: %v", err)
+		return
 	}
 
-	data := RequestLog{
-		RequestID: requestID,
-		Method:    c.Method(),
-		Path:      c.Path(),
-		Status:    c.Response().StatusCode(),
-		Duration:  time.Since(start).String(),
-		UserID:    userID,
-		Role:      role,
-	}
-
-	if m.logFunc != nil {
-		m.logFunc(data)
-	}
-
-	return err
+	logger.Printf("%s", payload)
 }
