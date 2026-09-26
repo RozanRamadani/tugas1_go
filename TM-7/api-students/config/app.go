07e5015b3f66d5336a7c98d354178fdc34793073
RozanRamadani <114994099+RozanRamadani@users.noreply.github.com>
2026-09-26 14:32:00 +0700

    Fix: Error Handler for 406

--- TM-7/api-students/config/app.go
+++ TM-7/api-students/config/app.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"errors"
 	"log"
 
 	"api-students/app/handler"
@@ -19,7 +20,9 @@ func NewApp(
 	perms *helper.PermissionSet,
 ) *fiber.App {
 
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		ErrorHandler: newErrorHandler(),
+	})
 
 	requestLogger := middleware.NewRequestLogger(
 		func(data middleware.RequestLog) {
@@ -48,4 +51,60 @@ func NewApp(
 	log.Println("route berhasil didaftarkan")
 
 	return app
+}
+
+type ErrorResponse struct {
+	Success   bool              `json:"success"`
+	Code      string            `json:"code"`
+	Message   string            `json:"message"`
+	RequestID string            `json:"request_id,omitempty"`
+	Fields    map[string]string `json:"fields,omitempty"`
+}
+
+func newErrorHandler() fiber.ErrorHandler {
+	return func(c *fiber.Ctx, err error) error {
+		status := fiber.StatusInternalServerError
+		code := helper.CodeInternal
+		message := "terjadi kesalahan pada server"
+		var fields map[string]string
+
+		var appErr *helper.AppError
+		if errors.As(err, &appErr) {
+			status = appErr.Status
+			code = appErr.Code
+			message = appErr.Message
+			fields = appErr.Fields
+		} else {
+			var fiberErr *fiber.Error
+			if errors.As(err, &fiberErr) {
+				status = fiberErr.Code
+				code = "ROUTER_ERROR"
+				message = fiberErr.Message
+			} else {
+				internal := helper.Internal(err)
+				status = internal.Status
+				code = internal.Code
+				message = internal.Message
+			}
+		}
+
+		if status >= 500 {
+			log.Printf("ERROR: %s: %s", code, message)
+		} else {
+			log.Printf("WARN: %s: %s", code, message)
+		}
+
+		var reqID string
+		if v, ok := c.Locals("requestid").(string); ok {
+			reqID = v
+		}
+
+		return c.Status(status).JSON(ErrorResponse{
+			Success:   false,
+			Code:      code,
+			Message:   message,
+			RequestID: reqID,
+			Fields:    fields,
+		})
+	}
 }
